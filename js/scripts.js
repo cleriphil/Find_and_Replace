cd17164fab280bb8793bf7da0852732638fae853
cleri philiastides <cleri.phil@gmail.com>
2015-05-29 10:20:34 -0700

    replace words with punctuation

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 var findAndReplace = function(sentence, wordToFind, wordToReplace) {
+  debugger;
+
+  sentence = sentence.replace(/[^\w\s]/g, ""); //do I need an anchor ^ before \w like [^\w\s]
   sentence = sentence.replace(new RegExp(wordToFind, "ig"), wordToReplace);
   return sentence;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,14 @@
 describe("findAndReplace", function() {
   it("finds and replaces a given word in a sentence", function() {
-    expect(findAndReplace("There is a tree.", "tree", "shrub")).to.equal("There is a shrub.");
+    expect(findAndReplace("There is a tree", "tree", "shrub")).to.equal("There is a shrub");
   });
   it("finds and replaces a repeated word in a sentence", function() {
     expect(findAndReplace("There is a tree tree tree", "tree", "shrub")).to.equal("There is a shrub shrub shrub");
   });
-  it("finds and replaces a word with capitalized letters in a sentence", function() {
+  it("finds and replaces a word in a sentence with captialization", function() {
     expect(findAndReplace("There is a tRee Tree TREE", "tree", "shrub")).to.equal("There is a shrub shrub shrub");
+  });
+  it("finds and replaces a word in a sentence with punctuation", function() {
+    expect(findAndReplace("There is a tree! tre*e t??ree!~~~!", "tree", "shrub")).to.equal("There is a shrub shrub shrub");
   });
 });

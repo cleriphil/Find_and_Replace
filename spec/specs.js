ff6f2311299b509cfd67bf53ccfa24ab2afb78be
cleri philiastides <cleri.phil@gmail.com>
2015-05-29 09:53:18 -0700

    replace words with captializeation, with RegExp i flag

--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,7 @@ describe("findAndReplace", function() {
   it("finds and replaces a repeated word in a sentence", function() {
     expect(findAndReplace("There is a tree tree tree", "tree", "shrub")).to.equal("There is a shrub shrub shrub");
   });
+  it("finds and replaces a word with capitalized letters in a sentence", function() {
+    expect(findAndReplace("There is a tRee Tree TREE", "tree", "shrub")).to.equal("There is a shrub shrub shrub");
+  });
 });

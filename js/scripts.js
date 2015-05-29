b1462249f571cccbbb980b15eaac6567d6ff25dc
cleri philiastides <cleri.phil@gmail.com>
2015-05-29 10:40:54 -0700

    replace words in sentences with punctuation, but will keep apostrophes, exclamation points, periods and commas

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 var findAndReplace = function(sentence, wordToFind, wordToReplace) {
-  debugger;
 
-  sentence = sentence.replace(/[^\w\s]/g, ""); //do I need an anchor ^ before \w like [^\w\s]
+  sentence = sentence.replace(/[^\w\s?,.!'"]/g, ""); //do I need an anchor ^ before \w like [^\w\s]
   sentence = sentence.replace(new RegExp(wordToFind, "ig"), wordToReplace);
   return sentence;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -9,6 +9,15 @@ describe("findAndReplace", function() {
     expect(findAndReplace("There is a tRee Tree TREE", "tree", "shrub")).to.equal("There is a shrub shrub shrub");
   });
   it("finds and replaces a word in a sentence with punctuation", function() {
-    expect(findAndReplace("There is a tree! tre*e t??ree!~~~!", "tree", "shrub")).to.equal("There is a shrub shrub shrub");
+    expect(findAndReplace("There is a tree*^% tre*e t%%ree!~~~!", "tree", "shrub")).to.equal("There is a shrub shrub shrub!!");
+  });
+  it("finds and replaces a word in a sentence with punctuation and keeps the period, exclamation point and question mark", function() {
+    expect(findAndReplace("There is a tre&&e tre*e... tre@#e!?", "tree", "shrub")).to.equal("There is a shrub shrub... shrub!?");
+  });
+  it("finds and replaces a word in a sentence with punctuation and keeps apostrophes and commas", function() {
+    expect(findAndReplace("There is a tree's tree, \"tree\"", "tree", "shrub")).to.equal("There is a shrub's shrub, \"shrub\"");
+  });
+  it("replaces a word that has an apostrophe after it", function() {
+    expect(findAndReplace("Sylvester's tree is green.", "sylvester", "Oliver")).to.equal("Oliver's tree is green.")
   });
 });
